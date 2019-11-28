2ded20a9a1db661628288b5fc7f1426df4c79ed8
jonathanjeansimon <monsieurs.js@gmail.com>
2019-11-28 05:12:06 +0100

    Creation de la class autoload  dans app

---
+++ app/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+*Class autoload
+*Permet de compiler les class 
+*/
+
+class autoloader{
+    
+    
+public function __construct(){
+    
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
