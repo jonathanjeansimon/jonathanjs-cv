4d0b6d8b107bf560bb18ff5c3a04f9b4d405c9ca
jonathanjeansimon <monsieurs.js@gmail.com>
2019-11-28 08:25:37 +0100

    Modification du gabarit

--- page/template/__gabarit.php
+++ page/template/__gabarit.php
@@ -9,27 +9,32 @@
 <body>
   
 <!-- Ouverture de la class HEADER  -->
-   <div class="header container-fluid">
+   <div class="header">
+    <div class="container">
      <div class="row">
 <!-- Ouverture de la class reseau -->
-        <div class="col-lg-6 reseau">
+        <div class="col-lg-2 logo">
+            <img src="../public/img/mrjs-min.png" alt="">
+        </div> 
+        
+         <div class="col-lg-2 reseau">
             
         </div> 
 <!-- Fermeture de la class reseau -->
 <!--  -------------------------   -->
 <!-- Ouverture de la class menu -->
-        <div class="col-lg-6 menu">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+        <div class="col-lg-8 menu">
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" id="menu-responsive" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <a class="nav-item nav-link" href="#">Accueil</a>
+      <a class="nav-item nav-link" href="#">Projet</a>
+      <a class="nav-item nav-link" href="#">Galerie</a>
+      <a class="nav-item nav-link" href="#">Contact</a>
     </div>
   </div>
 </nav>
@@ -38,16 +43,51 @@
 <!--  -------------------------   -->         
          
      </div>  
-       
+    </div>
    </div>
    
 <!-- Fermeture de la class HEADER -->
-   <section class="container">
+   <section class="container-fluid">
        
     <?= $content; ?>   
        
        
    </section>
+   <footer>
+       
+       
+       <div class="container">
+          <div class="row">
+          
+          <div class="col-lg-4">
+              
+              <p>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
+              </p>
+          </div>
+          
+             <div class="col-lg-4">
+              
+              <p>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
+              </p>
+          </div>
+           
+           
+              <div class="col-lg-4">
+              
+              <p>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
+              </p>
+          </div>
+           
+          </div>
+           
+       </div>
+       
+       
+       
+   </footer>
     
 </body>
 </html>
