3a243ee67cbb5921caabf66de4825e736f40d1b6
jonathanjeansimon <monsieurs.js@gmail.com>
2019-11-29 09:58:09 +0100

    Modification de plage Home

--- page/home.php
+++ page/home.php
@@ -6,7 +6,7 @@
   <div class="jumbotron jumbotron-fluid col-lg-12">
   <div class="container">
     <h1 class="display-4">Compétences</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <p class="lead">Autodidacte, je me forme seul j'aime apprendre et depasser mes limites</p>
   </div>
 </div>  
     
@@ -29,8 +29,13 @@
         <div class="card" style="width: 18rem;">
   <img src="../public/img/12.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">PROJET</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">BACK-END</h5>
+   <ul>
+   <li> PHP 7 - MYSQL</li>
+   <li> POO - MVC</li>
+   <li> ANGULAR - NODEJS</li>
+    <li> MANGODB</li>
+   </ul>
     <a href="#" class="btn btn-danger">Suite..</a>
   </div>
 </div>
@@ -43,10 +48,16 @@
     <div class="col-lg-4">
         
         <div class="card" style="width: 18rem;">
-   <img src="../public/img/12.jpg" class="card-img-top" alt="...">
+   <img src="../public/img/13.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">GALERIE</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">FRONT-END</h5>
+  <ul>
+    <li>HTML5 - CSS3 </li>
+    <li>JavaScript - Jquery</li>
+    <li>Flexbox - SASS</li>
+    <li>UX - Bootstrap</li>
+  </ul>
+
     <a href="#" class="btn btn-danger">Suite..</a>
   </div>
 </div>
@@ -56,10 +67,15 @@
      <div class="col-lg-4">
          
          <div class="card" style="width: 18rem;">
-  <img src="../public/img/12.jpg" class="card-img-top" alt="...">
+  <img src="../public/img/14.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">PAO - AUTRES</h5>
+ <ul>
+    <li>Photoshop - Lightroom </li>
+    <li>Illustrator  - After effect</li>
+    <li>Vegas pro - Adobe premier</li>
+    <li>Git - Wordpress</li>
+  </ul>
     <a href="#" class="btn btn-danger">Suite..</a>
   </div>
 </div>
