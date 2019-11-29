a73f5b06b3c372c41be6ef5fc365f6264a28ebfe
jonathanjeansimon <monsieurs.js@gmail.com>
2019-11-29 10:00:49 +0100

    Modification du gabarit mise en place du javascript

--- page/template/__gabarit.php
+++ page/template/__gabarit.php
@@ -143,6 +143,7 @@
     </div>
     </div>   
     </div>
-    <
+    <script type="text/javascript" src="../public/js/bootstrap.js"></script>
+     <script type="text/javascript" src="../public/js/jquery-3.4.1.min.js"></script>
 </body>
 </html>
