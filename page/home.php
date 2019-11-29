7e34296c17dbed30de3c2b6eda9da3ad5a6baf6c
jonathanjeansimon <monsieurs.js@gmail.com>
2019-11-29 09:09:32 +0100

    Modification de home et gabarit

--- page/home.php
+++ page/home.php
@@ -1,5 +1,73 @@
-<p>
+<div class="row">
     
-    *Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, necessitatibus numquam cumque consequuntur labore blanditiis, perferendis molestias ipsam rerum architecto eveniet aliquam pariatur sint, quos totam vero repellat facere.
     
-</p>
+    
+    
+  <div class="jumbotron jumbotron-fluid col-lg-12">
+  <div class="container">
+    <h1 class="display-4">Compétences</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
+   
+
+   
+   
+   <div class="row">
+    
+    <div class="col-lg-4">
+        <div class="card" style="width: 18rem;">
+  <img src="../public/img/12.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">PROJET</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Suite..</a>
+  </div>
+</div>
+        
+        
+        
+    </div>
+    
+    
+    <div class="col-lg-4">
+        
+        <div class="card" style="width: 18rem;">
+   <img src="../public/img/12.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">GALERIE</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Suite..</a>
+  </div>
+</div>
+        
+    </div>
+    
+     <div class="col-lg-4">
+         
+         <div class="card" style="width: 18rem;">
+  <img src="../public/img/12.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-danger">Suite..</a>
+  </div>
+</div>
+         
+         
+     </div>
+    
+    
+    
+    
+</div>
--- page/template/__gabarit.php
+++ page/template/__gabarit.php
@@ -9,21 +9,21 @@
 <body>
   
 <!-- Ouverture de la class HEADER  -->
-   <div class="header">
-    <div class="container">
+   <header>
+    <div class="container header">
      <div class="row">
 <!-- Ouverture de la class reseau -->
         <div class="col-lg-2 logo">
             <img src="../public/img/mrjs-min.png" alt="">
         </div> 
         
-         <div class="col-lg-2 reseau">
+         <div class="col-lg-4 reseau">
             
         </div> 
 <!-- Fermeture de la class reseau -->
 <!--  -------------------------   -->
 <!-- Ouverture de la class menu -->
-        <div class="col-lg-8 menu">
+        <div class="col-lg-6 menu">
             <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <a class="navbar-brand" id="menu-responsive" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -44,10 +44,40 @@
          
      </div>  
     </div>
-   </div>
+
+   </header>
+   
+   
+   <div class="container-fluid presentation">
+        
+<div class="container">  
+<div class="media row">
+  <img src="../public/img/ano.png" class="col-lg-4" alt="...">
+  <div class="media-body col-lg-8">
+    <h3 class="mt-0">Jean-simon Jonathan</h3>
+    <br/>
+     <h6>MON HISTOIRE : <span color=""></span>Mes débuts dans le developpement web </h6>
+<p></p>Avant de m’inscrire dans plusieurs CFA, je me suis forme seul au métier du web. De l’infographie au développement de site dynamique grâce au site
+« LE SITE DU ZERO » devenu à ce jour <a href="https://openclassrooms.com">Openclassroom.</a> </p>
+
+
+     
+     <button type="button" class="btn btn-secondary"> lire la suite... </button>
+     
+  </div>
+</div>   
+</div> 
+        
+        
+        
+    </div> 
+   
    
 <!-- Fermeture de la class HEADER -->
-   <section class="container-fluid">
+<!--  -------------------------   --> 
+   
+
+   <section class="container">
        
     <?= $content; ?>   
        
@@ -56,29 +86,44 @@
    <footer>
        
        
-       <div class="container">
+       <div class="container footer">
           <div class="row">
           
           <div class="col-lg-4">
               
-              <p>
-                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
+             
+             <h3>Contact</h3>
+ <p>
+
+<p>Telephone 1: +33 6 14 43 97 15</p>
+<p>Telephone 2: +33 7 83 63 10 72</p>
+<p>E-mail: jonathanjeansimon@gmail.com</p>
+
               </p>
           </div>
           
              <div class="col-lg-4">
               
-              <p>
-                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
-              </p>
+            <h3>Menu</h3>
+            <ul>
+                <li> ACCUEIL</li>
+                <li> PROJET</li>
+                <li> GALERIE</li>
+                <li> CONTACT</li>
+            </ul>
+            
           </div>
            
            
               <div class="col-lg-4">
               
-              <p>
-                  <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae iste ad ipsum reiciendis natus eaque corrupti odit voluptates in consectetur rem a nam assumenda earum deserunt, provident cumque officia eveniet.</span>
-              </p>
+               <h3>ZONE DE TRAVAIL</h3>
+            <ul>
+                <li> </li>
+                <li> PROJET</li>
+                <li> GALERIE</li>
+                <li> CONTACT</li>
+            </ul>
           </div>
            
           </div>
@@ -88,6 +133,16 @@
        
        
    </footer>
-    
+    <div class="bas-de-page">
+
+    <div class="container">
+       <div class="row">
+        <div class="col-lg-3"><p>© 2008–2019 Jean-simon Jonathan</p></div>
+        <div class="col-lg-6"></div>
+        <div class="col-lg-3"><p> User agreement  </p></div>
+    </div>
+    </div>   
+    </div>
+    <
 </body>
 </html>
